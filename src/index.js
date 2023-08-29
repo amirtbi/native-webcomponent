0478ff7a6da0e407a3d3d5a60fcd8f08812fdbc5
torabi <torabi46@gmail.com>
2023-08-29 16:18:04 +0330

    add modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import CustomNavbar from "./navbar.js";
 import CustomTooltip from "./tooltip.js";
+import CustomModal from "./modal-legacy.js";
 import ConfirmLink from "./confirm-link.js";
 
 customElements.define("custom-navbar", CustomNavbar);
 customElements.define("custom-tooltip", CustomTooltip);
+customElements.define("custom-modal", CustomModal);
