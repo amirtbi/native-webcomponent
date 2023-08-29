e9f6bb08e1c78483e872426f14522549a0ede13c
torabi <torabi46@gmail.com>
2023-08-29 11:00:00 +0330

    feat: create module type for web components

---
+++ src/confirm-link.js
@@ -0,0 +1,12 @@
+class ConfirmLink extends HTMLAnchorElement {
+  connectedCallback() {
+    this.addEventListener("click", (e) => {
+      if (!confirm("Do you confirm to leave?")) {
+        e.preventDefault();
+      }
+    });
+  }
+}
+
+// customElements.define("custom-link", ConfirmLink, { extends: "a" });
+export default ConfirmLink;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import CustomNavbar from "./navbar.js";
+import CustomTooltip from "./tooltip.js";
+import ConfirmLink from "./confirm-link.js";
+
+customElements.define("custom-navbar", CustomNavbar);
+customElements.define("custom-tooltip", CustomTooltip);
